dec844cc1cfa25ab9957090cd52e4a2bb09e5cfb
Vladimir Maljkovic <maljkovic.vladimir@gmail.com>
2021-03-01 13:55:46 +0100

    Added attributes, getters and setters

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,5 +1,55 @@
 package biblioteka;
 
 public class Knjiga {
-
+	
+	private String naslov;
+	private String autori;
+	private String isbn;
+	private String izdavac;
+	private int izdanje;
+	
+	public String getNaslov() {
+		return naslov;
+	}
+	
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	
+	public String getAutori() {
+		return autori;
+	}
+	
+	public void setAutori(String autori) {
+		this.autori = autori;
+	}
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	public String getIzdavac() {
+		return izdavac;
+	}
+	
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+	
+	public int getIzdanje() {
+		return izdanje;
+	}
+	
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	
+	
+	
+	
+	
 }
