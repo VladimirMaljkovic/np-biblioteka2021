203e216982741dab47fbd11934f10a498769dd2e
Vladimir Maljkovic <maljkovic.vladimir@gmail.com>
2021-03-01 14:41:53 +0100

    Added Autor class, adjusted Knjiga class to include Autor as a class

---
+++ src/biblioteka/Autor.java
@@ -0,0 +1,25 @@
+package biblioteka;
+
+public class Autor {
+	
+	private String ime;
+	private String prezime;
+	
+	public String getIme() {
+		return ime;
+	}
+	
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	
+	public String getPrezime() {
+		return prezime;
+	}
+	
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	
+	
+}
--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,9 +1,11 @@
 package biblioteka;
 
+import java.util.LinkedList;
+
 public class Knjiga {
 	
 	private String naslov;
-	private String autori;
+	private LinkedList <Autor> autori;
 	private String isbn;
 	private String izdavac;
 	private int izdanje;
@@ -16,14 +18,14 @@ public class Knjiga {
 		this.naslov = naslov;
 	}
 	
-	public String getAutori() {
+	public LinkedList<Autor> getAutori() {
 		return autori;
 	}
-	
-	public void setAutori(String autori) {
+
+	public void setAutori(LinkedList<Autor> autori) {
 		this.autori = autori;
 	}
-	
+
 	public String getIsbn() {
 		return isbn;
 	}
