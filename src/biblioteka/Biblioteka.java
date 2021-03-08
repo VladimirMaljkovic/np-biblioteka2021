127a1c34ed1e1db12c60c20499923d7bd18586f9
Vladimir Maljkovic <maljkovic.vladimir@gmail.com>
2021-03-08 12:59:48 +0100

    added logic control for Biblioteka (2nd option)

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -9,6 +9,10 @@ public class Biblioteka implements IBiblioteka {
 	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga==null)
+			throw new NullPointerException("knjiga ne sme biti null");
+		if(knjige.contains(knjiga))
+			throw new RuntimeException("knjiga vec postoji");
 		knjige.add(knjiga);
 	}
 
@@ -24,6 +28,8 @@ public class Biblioteka implements IBiblioteka {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		if(autor==null && isbn==null && naslov==null && izdavac==null)
+			return knjige;
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
 		
 		for(Knjiga knjiga: knjige)
