4f42e6dd5e88f08a894951cdd04f932af5ee18f3
Vladimir Maljkovic <maljkovic.vladimir@gmail.com>
2021-03-08 12:54:44 +0100

    added logic control in Biblioteka

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -9,11 +9,15 @@ public class Biblioteka implements IBiblioteka {
 	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga == null || knjige.contains(knjiga))
+			throw new RuntimeException("ne sme biti null ili duplikat");
 		knjige.add(knjiga);
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		if(knjiga==null || !knjige.contains(knjiga))
+			throw new RuntimeException("knjiga ne sme biti null, mora postojati");
 		knjige.remove(knjiga);
 	}
 
@@ -24,6 +28,10 @@ public class Biblioteka implements IBiblioteka {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		
+		if(autor==null && isbn==null && naslov==null && izdavac==null)
+			throw new RuntimeException("mora imati bar jedan kriterijum");
+		
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
 		
 		for(Knjiga knjiga: knjige)
