a783bc93a6e7a166a5ba5b09f800b6cda635ab74
Vladimir Maljkovic <maljkovic.vladimir@gmail.com>
2021-03-15 15:44:02 +0100

    Zapoceta dokumentacija IBiblioteke

--- src/biblioteka/interfaces/IBiblioteka.java
+++ src/biblioteka/interfaces/IBiblioteka.java
@@ -5,10 +5,30 @@ import java.util.LinkedList;
 import biblioteka.Autor;
 import biblioteka.Knjiga;
 
+/**
+ * Interfejs koji predstavlja biblioteku
+ * 
+ * @author Vladimir
+ *
+ */
 public interface IBiblioteka {
 	
+	/**
+	 * Dodaje novu knjigu u biblioteku.
+	 * 
+	 * @param knjiga - Knjiga koja se dodaje
+	 * 
+	 * @throws java.lang.NullPointerException ako je uneta knjiga null
+	 * 
+	 * @throws java.lang.RuntimeException ako uneta knjiga vec postoji u biblioteci (duplikat)
+	 */
 	public void dodajKnjigu(Knjiga knjiga);
 	
+	/**
+	 * Brise knjigu iz bibliteke.
+	 * 
+	 * @param knjiga - Knjiga koja treba da se obrise
+	 */
 	public void obrisiKnjigu(Knjiga knjiga);
 	
 	public LinkedList<Knjiga> vratiSveKnjige();
